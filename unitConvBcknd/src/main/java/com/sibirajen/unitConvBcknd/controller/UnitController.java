1eacc91f9cb96cd9811407bc446428c37135f6ba
sibirajen <sibirajen16@gmail.com>
2025-03-12 22:48:41 +0530

    Allowed CORS origin requests from all domains

--- unitConvBcknd/src/main/java/com/sibirajen/unitConvBcknd/controller/UnitController.java
+++ unitConvBcknd/src/main/java/com/sibirajen/unitConvBcknd/controller/UnitController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin
 public class UnitController {
     @PostMapping("/")
     public Response getData(@RequestBody Request request){
